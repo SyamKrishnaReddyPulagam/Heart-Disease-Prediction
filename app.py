2cc590fad9becf46fd96bb6267c3b6d5f951e915
Syam Krishna Reddy Pulagam <125567293+SyamKrishnaReddyPulagam@users.noreply.github.com>
2023-12-26 11:30:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,16 @@ sc = pickle.load(open('sc.pkl', 'rb'))
 
 st.header(" DISEASE PREDICTION ")
 
-Age=st.number_input("Age",min_value=0,max_value=100,value=10,step=1)
+col1,col2= st.columns(2)
+with col1:
+   Age=st.number_input("Age",min_value=0,max_value=100,value=10,step=1)
 
-sex=st.selectbox("Sex",['Male','Female'],index=0)
-if sex=="Male":
-    sex=1
-else:
-    sex=0
+with col2:
+   sex=st.selectbox("Sex",['Male','Female'],index=0)
+   if sex=="Male":
+      sex=1
+   else:
+      sex=0
 
 ChestPainType=st.selectbox("ChestPainType",['ASY','NAP','ATA','TA'],index=0)
 if ChestPainType=="ASY":
@@ -28,9 +31,12 @@ else:
 
 RestingBP=st.number_input("RestingBP",min_value=0,max_value=200,value=1,step=1)
 
-Cholesterol=st.number_input("Cholesterol",min_value=0,max_value=700,value=1,step=1)
+col3,col4= st.columns(2)
+with col3:
+   Cholesterol=st.number_input("Cholesterol",min_value=0,max_value=700,value=1,step=1)
 
-FastingBS=st.number_input("FastingBS",min_value=0,max_value=1,value=1,step=1)
+with col4:
+   FastingBS=st.number_input("FastingBS",min_value=0,max_value=1,value=1,step=1)
 
 RestingECG=st.selectbox("RestingECG",['Normal','LVH','ST'],index=0)
 if RestingECG=="":
@@ -40,21 +46,25 @@ elif RestingECG=="":
 else:
    RestingECG=2
 
-ExerciseAngina=st.selectbox("ExerciseAngina",['No','Yes'],index=0)
-if ExerciseAngina=="No":
-   ExerciseAngina=0
-else:
-   ExerciseAngina=1
+col5,col6,col7= st.columns(3)
+with col5:
+   ExerciseAngina=st.selectbox("ExerciseAngina",['No','Yes'],index=0)
+   if ExerciseAngina=="No":
+      ExerciseAngina=0
+   else:
+      ExerciseAngina=1
 
-Oldpeak=st.number_input("Oldpeak",min_value=-2.6,max_value=10.0,value=0.0,step=0.5)
+with col6:
+   Oldpeak=st.number_input("Oldpeak",min_value=-2.6,max_value=10.0,value=0.0,step=0.5)
 
-ST_Slope=st.selectbox("ST_Slope",['Flat','Up',"Down"],index=0)
-if ST_Slope=="Flat":
-   ST_Slope=1
-elif ST_Slope=="Up":
-   ST_Slope=2
-else:
-   ST_Slope=0
+with col7:
+   ST_Slope=st.selectbox("ST_Slope",['Flat','Up',"Down"],index=0)
+   if ST_Slope=="Flat":
+      ST_Slope=1
+   elif ST_Slope=="Up":
+      ST_Slope=2
+   else:
+      ST_Slope=0
 
 if st.button("PREDICT"):
     classifier=loaded_model.predict(sc.transform([[Age,sex,ChestPainType,RestingBP,Cholesterol,FastingBS,RestingECG,ExerciseAngina,Oldpeak,ST_Slope]]))
